6856e63d3b7e090db22b0edd61554c6e8a321d9c
ning <15996204@qq.com>
2022-04-10 23:23:51 +0800

    init

--- src/app.rs
+++ src/app.rs
@@ -1,30 +1,31 @@
 use eframe::{egui, epi};
 
+#[derive(Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+enum LeftPanel {
+    Page,
+    Component,
+    Element,
+}
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    // this how you opt-out of serialization of a member
-    #[cfg_attr(feature = "persistence", serde(skip))]
-    value: f32,
+    left_panel: LeftPanel,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            left_panel: LeftPanel::Page,
         }
     }
 }
 
 impl epi::App for TemplateApp {
     fn name(&self) -> &str {
-        "eframe template"
+        "Design Master"
     }
 
     /// Called once before the first frame.
@@ -34,6 +35,7 @@ impl epi::App for TemplateApp {
         _frame: &epi::Frame,
         _storage: Option<&dyn epi::Storage>,
     ) {
+        setup_custom_fonts(_ctx);
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         #[cfg(feature = "persistence")]
@@ -52,7 +54,7 @@ impl epi::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
-        let Self { label, value } = self;
+        let Self { left_panel } = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -70,49 +72,84 @@ impl epi::App for TemplateApp {
             });
         });
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+        egui::SidePanel::left("left_panel")
+            .resizable(true)
+            .default_width(150.0)
+            .width_range(80.0..=200.0)
+            .show(ctx, |ui| {
+                egui::TopBottomPanel::top("side_choose_panel")
+                    .resizable(false)
+                    .min_height(0.0)
+                    .show_inside(ui, |ui| {
+                        ui.horizontal(|ui| {
+                            ui.selectable_value(left_panel, LeftPanel::Page, "页面");
+                            ui.selectable_value(left_panel, LeftPanel::Component, "组件");
+                            ui.selectable_value(left_panel, LeftPanel::Element, "元素");
+                        });
+                    });
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
+                match left_panel {
+                    LeftPanel::Page => {
+                        ui.heading("页面列表");
+                    }
+                    LeftPanel::Component => {
+                        ui.heading("组件列表");
+                    }
+                    LeftPanel::Element => {
+                        ui.heading("元素列表");
+                    }
+                }
             });
 
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
-                });
+        egui::SidePanel::right("right_bar")
+            .default_width(350.0)
+            .resizable(false)
+            .show(ctx, |ui| {
+                ui.heading("组件属性");
             });
-        });
+
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
+            ui.heading("设计大师");
+            ui.end_row();
+            ui.label("像大师一样设计：");
+            ui.end_row();
+            ui.label("高复用 -- 直接使用他人写好的组件/自定义组件");
+            ui.end_row();
+            ui.label("原型即代码 -- 直接导出Web/桌面/小程序/手机组件代码");
+            ui.end_row();
+            ui.label("高性能 -- 立即响应");
+            ui.end_row();
+            ui.label("低存储 -- 导出文件只需几十K而不是几个G");
         });
-
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
-        }
     }
+}
+
+fn setup_custom_fonts(ctx: &egui::Context) {
+    // Start with the default fonts (we will be adding to them rather than replacing them).
+    let mut fonts = egui::FontDefinitions::default();
+
+    // Install my own font (maybe supporting non-latin characters).
+    // .ttf and .otf files supported.
+    fonts.font_data.insert(
+        "my_font".to_owned(),
+        egui::FontData::from_static(include_bytes!("../fonts/wqy-microhei.ttc")),
+    );
+
+    // Put my font first (highest priority) for proportional text:
+    fonts
+        .families
+        .entry(egui::FontFamily::Proportional)
+        .or_default()
+        .insert(0, "my_font".to_owned());
+
+    // Put my font as last fallback for monospace:
+    fonts
+        .families
+        .entry(egui::FontFamily::Monospace)
+        .or_default()
+        .push("my_font".to_owned());
+
+    // Tell egui to use these fonts:
+    ctx.set_fonts(fonts);
 }
